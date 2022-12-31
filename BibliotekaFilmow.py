19c5928e4fe0a720c2057b3c17a6a4632497f24f
Maciej Kolakowski <maciej.kolakowski@gmail.com>
2022-12-31 19:21:24 +0100

    Add probka filmow i seriali

--- BibliotekaFilmow.py
+++ BibliotekaFilmow.py
@@ -26,3 +26,17 @@ class Seriale(Filmy):
         
     def __repr__(self):
         return f"Serial ({self.tytul},{self.rok_wydania}, S{self.numer_sezonu:02d}E{self.numer_odcinka:02d}, {self.liczba_wyswietlen})"
+
+F1=Filmy('Pulp Fiction', 1994, 'sensacja', 0)
+F2=Filmy('Rambo', 1984, 'dramat', 0)
+F3=Filmy('Commando', 1983, 'sensacja', 0)
+F4=Filmy('Akademia Policyjna', 1985, 'komedia', 0)
+F5=Filmy('Mamma Mia', 2008, 'musical', 0)
+S1=Seriale('Friends', 1995, 'komedia', 0, 1, 1 )
+S2=Seriale('Friends', 1995, 'komedia', 0, 2, 1 )
+S3=Seriale('Friends', 1995, 'komedia', 0, 3, 1 )
+S4=Seriale('Alternatywy 4', 1986, 'komedia', 0, 1, 1 )
+S5=Seriale('Alternatywy 4', 1986, 'komedia', 0, 2, 1 )
+S6=Seriale('Chyłka', 2015, 'sensacja', 0, 1, 1 )
+S7=Seriale('Chyłka', 2015, 'sensacja', 0, 2, 1 )
+
